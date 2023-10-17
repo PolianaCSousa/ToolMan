bac75ce5a5b353c3a52c8c52c4305375641b25cd
Poliana <polianasamonte@gmail.com>
2023-10-17 13:03:53 -0300

    Criando os controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TimeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +27,6 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+
+Route::resource('times', TimeController::class);
